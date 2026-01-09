5487f95a3a58be1f3d6f57bcf384c42739a14932
nassim99 <nassimbenmustpha99@gamil.com>
2026-01-09 21:57:42 +0100

    fix collection('user')

--- project_flutter/lib/features/profile/user_service.dart
+++ project_flutter/lib/features/profile/user_service.dart
@@ -5,7 +5,7 @@ class UserService {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   Future<void> updateUser(User user) async {
-    await _firestore.collection('users').doc(user.id).update({
+    await _firestore.collection('user').doc(user.id).update({
       'username': user.username,
       'email': user.email,
     });
